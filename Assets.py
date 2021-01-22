baf1628f48ed19f5fbfcbc3e9ec5dfd823ddc5b0
afacio <afacio@users.noreply.github.com>
2021-01-22 13:49:51 +0100

    Update Assets.py

--- Assets.py
+++ Assets.py
@@ -1,10 +1,10 @@
 import pygame
 SIZE = 80
 
-WHITE_PAWN = pygame.transform.scale(pygame.image.load("pawn_white.png"), (SIZE, SIZE))
-BLACK_PAWN = pygame.transform.scale(pygame.image.load("pawn_black.png"), (SIZE, SIZE))
-WHITE_QUEEN = pygame.transform.scale(pygame.image.load("queen_white.png"), (SIZE, SIZE))
-BLACK_QUEEN = pygame.transform.scale(pygame.image.load("queen_black.png"), (SIZE, SIZE))
+WHITE_PAWN = pygame.transform.scale(pygame.image.load("../pawn_white.png"), (SIZE, SIZE))
+BLACK_PAWN = pygame.transform.scale(pygame.image.load("../pawn_black.png"), (SIZE, SIZE))
+WHITE_QUEEN = pygame.transform.scale(pygame.image.load("../queen_white.png"), (SIZE, SIZE))
+BLACK_QUEEN = pygame.transform.scale(pygame.image.load("../queen_black.png"), (SIZE, SIZE))
 
 SIZE = 80
 BOARD_SIZE = 8
